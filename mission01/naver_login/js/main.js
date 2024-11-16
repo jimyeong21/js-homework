20255a73a70fdde5a97067281285ba5fffd7b885
jimyeong21 <jimyeong21@gmail.com>
2024-11-16 16:25:12 +0900

    네이버 로그인 과제 제출

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,36 +1,66 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
+
+const userEmail = document.querySelector(".user-email-input");
+const userPassword = document.querySelector(".user-password-input");
+const loginForm = document.querySelector(".login-form");
+
+// 1. email 정규표현식을 사용한 validation
+
+function checkEmail() {
+  let value = userEmail.value;
+  if (!emailReg(value)) {
+    userEmail.classList.add("is--invalid");
+  } else {
+    userEmail.classList.remove("is--invalid");
+  }
 }
 
+userEmail.addEventListener("keydown", checkEmail);
+
+// 2. pw 정규표현식을 사용한 validation
+
+function checkPassword() {
+  let value = userPassword.value;
+  if (!pwReg(value)) {
+    userPassword.classList.add("is--invalid");
+  } else {
+    userPassword.classList.remove("is--invalid");
+  }
+}
+
+userPassword.addEventListener("keydown", checkPassword);
+
+// 3. 상태 변수 관리
 /*
-
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
-
+범쌤 안녕하세요.. 상태 변수 관리가 무엇인지 잘 모르겠어요.. ㅠㅠ 
+오늘도 화이팅입니다! 럭키비키잔앙
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// 4. 로그인 버튼을 클릭시 조건처리
 
-  return re.test(String(text).toLowerCase())
+function clickLogin(e) {
+  e.preventDefault();
+  if (userEmail.value === user.id && userPassword.value === user.pw) {
+    window.location.href = "welcome.html";
+    // console.log("success");
+  } else {
+    alert("아이디와 비밀번호를 다시 확인해주세요.");
+    // console.log("fail");
+  }
 }
 
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+loginForm.addEventListener("submit", clickLogin);
+
+function emailReg(text) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   return re.test(String(text).toLowerCase());
 }
 
-
-
-
-
-
-
-
-
-
-
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
