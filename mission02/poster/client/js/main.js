a4d2b59b5e9472a5d86de19184ce8eed1760f83b
jimyeong21 <jimyeong21@gmail.com>
2024-11-24 11:05:46 +0900

    포스터 과제 제출

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,44 @@
 
 */
 
+const body = document.querySelector("body");
+const navigation = document.querySelector("nav ul");
+const text = document.querySelector(".nickName");
+const img = document.querySelector(".visual img");
 
+function setBgColor(color1, color2 = "#000") {
+  body.style.background = `linear-gradient(to bottom, ${color1}, ${color2})`;
+}
 
+function setImage(imgName, imgAlt) {
+  img.src = imgName;
+  img.alt = imgAlt;
+}
 
+function setNameText(name) {
+  text.textContent = name;
+}
 
+function handler(e) {
+  e.preventDefault();
 
+  const target = e.target.closest("li");
+  const list = [...this.children];
 
+  if (!target) return;
 
+  const index = target.dataset.index;
+  const dataIndex = data[index - 1];
 
+  list.forEach((li) => li.classList.remove("is-active"));
 
+  setBgColor(dataIndex.color[0], dataIndex.color[1]);
 
+  setImage(`./assets/${dataIndex.name}.jpeg`, dataIndex.alt);
 
+  setNameText(`${dataIndex.name}`);
 
+  target.classList.add("is-active");
+}
 
+navigation.addEventListener("click", handler);
